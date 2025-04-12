ea4bf589225539ff95b1c265f0a1df0cc9a7dc66
ismaelxyz <50039280+ismaelxyz@users.noreply.github.com>
2025-04-13 00:32:21 +0100

    feat:  support for wasm, pons  and yandex implemented

--- src/translator/wasm/mod.rs
+++ src/translator/wasm/mod.rs
@@ -8,6 +8,8 @@ mod microsoft;
 mod mymemory;
 mod qcri;
 mod papago;
+mod pons;
+mod yandex;
 
 use wasm_bindgen::prelude::*;
 pub use google::*;
@@ -18,6 +20,8 @@ pub use microsoft::*;
 pub use mymemory::*;
 pub use qcri::*;
 pub use papago::*;
+pub use pons::*;
+pub use yandex::*;
 
 
 #[derive(
@@ -58,97 +62,3 @@ pub struct Translator {
     target: JsValue,
 }
 
-
-
-#[derive(Clone, Default)]
-#[wasm_bindgen]
-pub struct Pons {
-    translator: Translator
-}
-
-#[wasm_bindgen]
-impl Pons {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    #[wasm_bindgen(setter, js_name = setSource)]
-    pub fn set_source(&mut self, source: &str) {
-        self.translator.source = JsValue::from(source);
-    }
-
-    #[wasm_bindgen(setter, js_name = setTarget)]
-    pub fn set_target(&mut self, target: &str) {
-        self.translator.target = JsValue::from(target);
-    }
-
-    #[wasm_bindgen(getter, js_name = getSource)]
-    pub fn source(&self) -> JsValue {
-        self.translator.source.clone()
-    }
-
-    #[wasm_bindgen(getter, js_name = getTarget)]
-    pub fn target(&self) -> JsValue {
-        self.translator.target.clone()
-    }
-}
-
-
-
-#[derive(Clone, Default)]
-#[wasm_bindgen]
-pub struct Yandex {
-    translator: Translator,
-    // String
-    api_key: JsValue,
-}
-
-#[wasm_bindgen]
-impl Yandex {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    #[wasm_bindgen(setter, js_name = setSource)]
-    pub fn set_source(&mut self, source: &str) {
-        self.translator.source = JsValue::from(source);
-    }
-
-    #[wasm_bindgen(setter, js_name = setTarget)]
-    pub fn set_target(&mut self, target: &str) {
-        self.translator.target = JsValue::from(target);
-    }
-
-    #[wasm_bindgen(getter, js_name = getSource)]
-    pub fn source(&self) -> JsValue {
-        self.translator.source.clone()
-    }
-
-    #[wasm_bindgen(getter, js_name = getTarget)]
-    pub fn target(&self) -> JsValue {
-        self.translator.target.clone()
-    }
-
-    #[wasm_bindgen(setter, js_name = setApiKey)]
-    pub fn set_api_key(&mut self, api_key: &str) {
-        self.api_key = JsValue::from(api_key);
-    }
-
-    #[wasm_bindgen(getter, js_name = getApiKey)]
-    pub fn api_key(&self) -> JsValue {
-        self.api_key.clone()
-    }
-    
-    // #[wasm_bindgen]
-    // pub async fn translate(self, text: &str) -> Result<JsValue, JsValue> {
-    //     let translator = crate::Translator::from(self);
-
-    //     translator
-    //         .translate(text)
-    //         .await
-    //         .map_err(|err| JsValue::from(err.to_string()))
-    //         .map(|ok| <JsValue as JsValueSerdeExt>::from_serde(&ok).unwrap())
-    // }
-}
---
+++ src/translator/wasm/yandex.rs
@@ -0,0 +1,77 @@
+use gloo_utils::format::JsValueSerdeExt;
+use wasm_bindgen::prelude::*;
+
+#[derive(Clone, Default)]
+#[wasm_bindgen]
+pub struct Yandex {
+    translator: super::Translator,
+    // String
+    api_key: JsValue,
+}
+
+#[wasm_bindgen]
+impl Yandex {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[wasm_bindgen(setter, js_name = setSource)]
+    pub fn set_source(&mut self, source: &str) {
+        self.translator.source = JsValue::from(source);
+    }
+
+    #[wasm_bindgen(setter, js_name = setTarget)]
+    pub fn set_target(&mut self, target: &str) {
+        self.translator.target = JsValue::from(target);
+    }
+
+    #[wasm_bindgen(getter, js_name = getSource)]
+    pub fn source(&self) -> JsValue {
+        self.translator.source.clone()
+    }
+
+    #[wasm_bindgen(getter, js_name = getTarget)]
+    pub fn target(&self) -> JsValue {
+        self.translator.target.clone()
+    }
+
+    #[wasm_bindgen(setter, js_name = setApiKey)]
+    pub fn set_api_key(&mut self, api_key: &str) {
+        self.api_key = JsValue::from(api_key);
+    }
+
+    #[wasm_bindgen(getter, js_name = getApiKey)]
+    pub fn api_key(&self) -> JsValue {
+        self.api_key.clone()
+    }
+
+    #[wasm_bindgen]
+    pub async fn translate(self, text: &str) -> Result<JsValue, JsValue> {
+        let translator = crate::Translator::from(self);
+
+        translator
+            .translate(text)
+            .await
+            .map_err(|err| JsValue::from(err.to_string()))
+            .map(|ok| <JsValue as JsValueSerdeExt>::from_serde(&ok).unwrap())
+    }
+}
+
+impl From<Yandex> for crate::Translator {
+    #[inline(always)]
+    fn from(wasm_translator: Yandex) -> crate::Translator {
+        let Yandex {
+            translator: super::Translator { source, target },
+            api_key,
+        } = wasm_translator;
+
+        let source = source.as_string().unwrap();
+        let target = target.as_string().unwrap();
+        let api_key = api_key.as_string().unwrap();
+
+        let engine = crate::Engine::Yandex { api_key };
+
+        crate::Translator::with_engine(&source, &target, engine)
+    }
+}
